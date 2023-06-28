1484d1f87ccbc84c84b3c0f6d7994e29b05ea819
Poohbear83 <124511211+Poohbear83@users.noreply.github.com>
2023-06-27 19:35:01 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+import Main from './Components/Main';
+import Header from './Components/Header';
+import Nav from './Components/Nav';
+import Footer from './Components/Footer';
+import './App.css';
+
+function App() {
+  return (
+    <>
+   <Header/>
+   <Nav/>
+   <Main/>
+   <Footer/>   
+    </>
+  );
+}
+
+export default App;
---
+++ Components/ConfirmedBooking.js
@@ -0,0 +1,8 @@
+export default function ConfirmedBooking(props) {
+    return (
+        <div>
+            <h1 className="markazi subtitle">The booking has been confirmed!</h1>
+            <p>Thank you for eating at Little Lemon.</p>
+        </div>
+    )
+}
